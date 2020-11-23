d8621b48d4523dc0ca2fe239b199c3d464a8044b
Philipp <ph.schaefer@protonmail.com>
2020-11-23 21:51:34 +0100

    Create a class Transaction

---
+++ lib/transaction.dart
@@ -0,0 +1,12 @@
+import 'dart:ffi';
+
+import 'package:flutter/foundation.dart';
+
+class Transaction {
+  @required String id;
+  @required String title;
+  @required double amount;
+  @required DateTime date;
+
+  Transaction({this.id, this.title, this.amount, this.date});
+}
