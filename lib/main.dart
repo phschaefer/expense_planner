c88c1dd4f5357403daf6feac322365ef8931cc0f
Philipp <ph.schaefer@protonmail.com>
2020-11-28 07:33:09 +0100

    Adding a ChartBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expense_planner/widgets/chart.dart';
 import 'package:expense_planner/widgets/new_transaction.dart';
 import 'package:flutter/material.dart';
 
@@ -19,20 +20,20 @@ class MyApp extends StatelessWidget {
         accentColor: Colors.grey,
         fontFamily: 'Quicksand',
         textTheme: ThemeData.light().textTheme.copyWith(
-          headline6: TextStyle(
-            fontFamily: 'OpenSans',
-            fontWeight: FontWeight.bold,
-            fontSize: 18,
-          ),
-        ),
+              headline6: TextStyle(
+                fontFamily: 'OpenSans',
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+            ),
         appBarTheme: AppBarTheme(
           textTheme: ThemeData.light().textTheme.copyWith(
-            headline6: TextStyle(
-                fontFamily: 'OpenSans',
-                fontSize: 20,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
+                headline6: TextStyle(
+                  fontFamily: 'OpenSans',
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
         ),
       ),
       home: MyHomePage(),
@@ -47,19 +48,29 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final List<Transaction> _transactions = [
-    // Transaction(
-    //   id: 't1',
-    //   title: 'New Shoes',
-    //   amount: 69.99,
-    //   date: DateTime.now(),
-    // ),
-    // Transaction(
-    //   id: 't2',
-    //   title: 'Weekly Groceries',
-    //   amount: 16.53,
-    //   date: DateTime.now(),
-    // ),
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Weekly Groceries',
+      amount: 16.53,
+      date: DateTime.now(),
+    ),
   ];
+
+  List<Transaction> get _recentTransactions {
+    return _transactions.where(
+      (element) => element.date.isAfter(
+        DateTime.now().subtract(
+          Duration(days: 7),
+        ),
+      ),
+    ).toList();
+  }
 
   void addTransaction(String title, double amount) {
     final newTransaction = Transaction(
@@ -89,9 +100,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-            'Expense Planner'
-        ),
+        title: Text('Expense Planner'),
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.add),
@@ -104,16 +113,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // mainAxisAlignment: MainAxisAlignment.spaceAround,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Container(
-              child: Card(
-                color: Colors.blue,
-                child: Container(
-                  child: Text('Chart!'),
-                  width: double.infinity,
-                ),
-                elevation: 5,
-              ),
-            ),
+            Chart(_recentTransactions),
             TransactionList(_transactions),
           ],
         ),
---
+++ lib/widgets/chart.dart
@@ -0,0 +1,50 @@
+import 'package:expense_planner/models/transaction.dart';
+import 'package:expense_planner/widgets/bar.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class Chart extends StatelessWidget {
+  final List<Transaction> recentTransaction;
+
+  Chart(this.recentTransaction);
+
+  List<Map<String, Object>> get groupTransactionsValues {
+    return List.generate(7, (index) {
+      double sum = 0.0;
+
+      final weekDay = DateTime.now().subtract(
+        Duration(days: index),
+      );
+
+      recentTransaction.forEach((element) {
+        if (element.date.day == weekDay.day &&
+            element.date.month == weekDay.month &&
+            element.date.year == weekDay.year) sum += element.amount;
+      });
+
+      return {
+        'day': DateFormat.E().format(weekDay).substring(0, 1),
+        'amount': sum
+      };
+    });
+  }
+
+  double get maxSpending {
+    return groupTransactionsValues.fold(0.0, (sum, item) => sum + item['amount']);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print(groupTransactionsValues);
+    return Card(
+      elevation: 5,
+      margin: EdgeInsets.all(20),
+      child: Row(
+        children: groupTransactionsValues
+            .map(
+              (data) => Bar(data['day'], data['amount'], (data['amount'] as double) / maxSpending),
+      ).toList(),
+      ),
+    );
+  }
+}
