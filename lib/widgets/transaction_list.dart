df63b6bc40d7c01afdc9c50ab3d2bb0dc0adfdac
Philipp <ph.schaefer@protonmail.com>
2020-12-05 00:25:13 +0100

    Adding a ListTile

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -30,53 +30,26 @@ class TransactionList extends StatelessWidget {
           ),
         ],
       ) :ListView.builder(
-        itemBuilder: (value, index) =>
-          Card(
-            child: Row(
-              children: <Widget>[
-                Container(
-                  margin: EdgeInsets.symmetric(
-                    vertical: 10,
-                    horizontal: 15,
-                  ),
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Theme.of(context).primaryColor,
-                      width: 2,
-                    ),
-                  ),
-                  padding: EdgeInsets.all(10),
-                  child: Text(
-                    '\$ ${transactions[index].amount.toStringAsFixed(2)}',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      color: Theme.of(context).primaryColor,
-                    ),
-                  ),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      transactions[index].title,
-                      style: Theme.of(context).textTheme.headline6,
-                    ),
-                    Text(
-                      DateFormat('d.MM.y').format(transactions[index].date),
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 13,
-                        color: Colors.grey,
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
+        itemBuilder: (value, index) => Card(
+          elevation: 5,
+          margin: EdgeInsets.symmetric(
+            vertical: 8,
+            horizontal: 5,
           ),
-        itemCount: transactions.length,
-      ),
+          child: ListTile(
+                  leading: CircleAvatar(
+                    radius: 30,
+                    child: Padding(
+                      padding: const EdgeInsets.all(6.0),
+                      child: Text('\$${transactions[index].amount}'),
+                    ),
+                  ),
+            title: Text(transactions[index].title, style: Theme.of(context).textTheme.headline6,),
+            subtitle: Text(DateFormat.yMMMd().format(transactions[index].date)),
+                ),
+        ),
+              itemCount: transactions.length,
+            ),
     );
   }
 }
