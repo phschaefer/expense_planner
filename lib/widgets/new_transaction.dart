6400a791d57c03f972c567c809e48bfd664caf29
Philipp <ph.schaefer@protonmail.com>
2020-12-26 09:57:31 +0100

    code formatting

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,7 +1,8 @@
-import '../widgets/adaptive_flat_button.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+
+import '../widgets/adaptive_flat_button.dart';
 
 class NewTransaction extends StatefulWidget {
   final Function addTransaction;
